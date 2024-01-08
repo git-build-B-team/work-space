54dc0912fefb189f394889dd09433a175a7cbde1
sakamotoayaka <133198004+sakamotoayaka@users.noreply.github.com>
2024-01-08 13:39:37 +0900

    topページとドリンクメニューを作成しました

---
+++ style.js
@@ -0,0 +1,9 @@
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
